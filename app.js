8208633d190ed5a4d02b123ca00a657ee50ae2fa
atmanad <atmanad0001@gmail.com>
2020-11-17 18:25:51 +0530

    Heroku testing #4

--- app.js
+++ app.js
@@ -87,9 +87,9 @@ app.post('/api/appointment', (req,res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static('client/build'))
+  app.use(express.static('my-doc/build'))
   app.get("*",(req,res)=>{
-      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+      res.sendFile(path.resolve(__dirname,'my-doc','build','index.html'))
   })
 }
 app.listen(process.env.PORT || 5000, () => console.log('server running on port 5000'));
